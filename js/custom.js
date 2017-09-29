bd00510b0950a4481d285787ea4670759c6ae7bd
utsavgarg <utsav1996@ymail.com>
2017-09-29 14:05:14 +0800

    login page

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+function login() {
+	var uname = document.getElementById('exampleInputEmail1').value;
+	var password = document.getElementById('exampleInputPassword1').value;
+
+	if (uname="admin" && password=="admin"){
+		window.location.replace('admin.html');
+		$("#loginError").css("display", "none");
+	}
+	else if (uname="abc" && password=="abc"){
+		window.location.replace('user.html');
+		$("#loginError").css("display", "none");
+	}
+	else{
+		$("#loginError").css("display", "block");
+	}
+}
+
